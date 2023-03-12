aed499a53050243c0b7cbb46fd402825eab5a749
dibssie <npdibbfuller@gmail.com>
2023-03-12 17:25:46 +0000

    edited main to use the new features and test the wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include <Arduino.h>
-#include <displayDriver.h>
+#include "displayDriver.h"
+#include "wifiDriver.h"
 
 void setup()
 {
+    Serial.begin(115200);
+    for (size_t i = 5; i > 0; i--)
+    {
+        Serial.printf("Starting in %d\n", i);
+        delay(1000);
+    }
+    
     displayDriver displayDriver;
+    wifiDriver wifiDriver;
+
+    displayDriver.LOG("About to connect to WiFi");
+    wifiDriver.wifiConnect();
+    displayDriver.LOG("Connected!!");
 }
 
 void loop()
 {
-
+    delay(1000);
 }
