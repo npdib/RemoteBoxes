358a6fb3e8460d969904a96ea70e6cb8e6b6fc8b
dibssie <npdibbfuller@gmail.com>
2023-03-25 20:00:31 +0000

    fixed instability with wifi connection

--- src/wifiDriver.cpp
+++ src/wifiDriver.cpp
@@ -11,19 +11,20 @@ wifiDriver::wifiDriver(void)
     int semiColon = mCredentials.find(';');
     mSSID = mCredentials.substr(0, semiColon);
     mPwd = mCredentials.substr(semiColon+1); 
-
     Serial.printf("SSID: %s\nPassword: %s\n", mSSID.c_str(), mPwd.c_str());
 }
 
 void wifiDriver::wifiConnect(void)
 {
-    Serial.printf("SSID: %s\nPassword: %s\n", mSSID.c_str(), mPwd.c_str());
-
-    if (WiFi.status() != WL_CONNECTED)
+    if (WiFi.status() == 255)
     {
         WiFi.begin(mSSID.c_str(), mPwd.c_str());
     }
-
+    else
+    {
+        WiFi.reconnect();
+    }
+    
     while (WiFi.status() != WL_CONNECTED)
     {
         Serial.printf("Connecting to WiFi, please wait... Current status %d\n", WiFi.status());
