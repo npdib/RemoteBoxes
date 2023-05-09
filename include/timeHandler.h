a2f7a8ca9ea115e1626f8b64c964a9dce7b8b981
egnicholasdibb-fuller <nicholasdibb-fuller@egtechnology.co.uk>
2023-05-09 12:09:30 +0100

    fixed multiple definition error

--- include/timeHandler.h
+++ include/timeHandler.h
@@ -5,10 +5,6 @@
 
 #define ONE_HOUR 3600000
 
-const char* ntpServer1 = "pool.ntp.org";
-const char* ntpServer2 = "time.nist.gov";
-const char* time_zone = "CET-1CEST,M3.5.0,M10.5.0/3";  // TimeZone rule for Europe/Rome including daylight adjustment rules (optional)
-
 void configureRTC(void);
 void setTime(void);
 void reFetchTime(void);
--- src/timeHandler.cpp
+++ src/timeHandler.cpp
@@ -3,6 +3,10 @@
 ESP32Time rtc;
 unsigned long fetch_time_timer = 0;
 
+const char* ntpServer1 = "pool.ntp.org";
+const char* ntpServer2 = "time.nist.gov";
+const char* time_zone = "CET-1CEST,M3.5.0,M10.5.0/3";  // TimeZone rule for Europe/Rome including daylight adjustment rules (optional)
+
 void configureRTC(void)
 {
     configTzTime(time_zone, ntpServer1, ntpServer2);
